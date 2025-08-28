e8a850a1eea457764f1e99d57d2465fdd335f620
João Guilherme Stumpf Fernandes <guist4@proton.me>
2025-08-27 22:21:07 -0300

    js finalizado

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function girardado(){
-    
+      const dice = document.querySelector("section");
+  const result = document.getElementById("resultado");
+
+  const diceFaces = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
+  const randomIndex = Math.floor(Math.random() * 6);
+
+  dice.textContent = diceFaces[randomIndex];
+  result.textContent = `Você tirou ${randomIndex + 1}!`;
 }
